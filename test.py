b6017caea0a077b6bec75f219214db8e3e6ad5f0
Afaq Ahmed <afaq704@gmail.com>
2022-03-18 23:13:57 +0530

    test.py
    test case

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt, timedelta
 import os
 import numpy as np
 
-from main import question_five, question_one, question_two
+from main import question_five, question_four, question_one, question_two
 
 
 def test_question_one():
@@ -45,8 +45,43 @@ def test_question_two():
     df.to_csv("test_airports.csv")
 
     result = question_two("test_flights.csv", "test_airports.csv")
-
+    os.remove("test_flights.csv")
+    os.remove("test_airports.csv")
     assert result == len(list(set(list(origin))))
+
+
+def test_question_four():
+    flights = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    weights_a = np.random.randint(50, 100, 13)
+    weights_b = np.random.randint(1, 50, 13)
+    rows = []
+    manufacturers = []
+    for i in range(26):
+        if i % 2:
+            weight = weights_a[i // 2]
+            manufacturers.append([flights[i] * 3, "MANUFACTURER_A"])
+        else:
+            weight = weights_b[i // 2]
+            manufacturers.append([flights[i] * 3, "MANUFACTURER_B"])
+        for j in range(weight):
+            dep_delay = np.random.randint(1, 20)
+            arr_delay = np.random.randint(1, 20)
+            rows.append([flights[i] * 3, dep_delay, arr_delay])
+    rows = np.array(rows)
+    manufacturers = np.array(manufacturers)
+    np.random.shuffle(rows)
+    np.random.shuffle(manufacturers)
+    df = pd.DataFrame(rows, columns=["tailnum", "dep_delay", "arr_delay"])
+    df.to_csv("test_flights.csv")
+
+    df = pd.DataFrame(manufacturers, columns=["tailnum", "manufacturer"])
+    df.to_csv("test_manufacturer.csv")
+
+    result = question_four("test_flights.csv", "test_manufacturer.csv")
+
+    os.remove("test_flights.csv")
+    os.remove("test_manufacturer.csv")
+    assert result == "MANUFACTURER_A"
 
 
 def test_question_five():
@@ -66,5 +101,6 @@ def test_question_five():
     df.to_csv("test_airports.csv")
 
     result = question_five("test_flights.csv", "test_airports.csv")
-
+    os.remove("test_flights.csv")
+    os.remove("test_airports.csv")
     assert list(result) == sorted([codes[idx], codes_rev[idx]])
